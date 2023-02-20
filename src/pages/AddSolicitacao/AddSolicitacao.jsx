491ba1408b4c2d81fffd0167e662547286647d15
YDan08 <dasyamashita@gmail.com>
2023-02-20 16:23:19 -0300

    feat: adicionar api e rules no cadastro de solicitacao

--- src/pages/AddSolicitacao/AddSolicitacao.jsx
+++ src/pages/AddSolicitacao/AddSolicitacao.jsx
@@ -1,14 +1,37 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
-import { Button, Col, Form, Input, Row, Typography } from 'antd';
-import { Link } from 'react-router-dom';
+import { Button, Col, Form, Input, message, Row, Typography } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
 import LayoutDefault from '../../components/LayoutDefault';
 import Categoria from '../../components/Categoria';
 import TituloCategoria from '../../components/TituloCategoria';
+import BotaoForm from '../../components/BotaoForm';
+import { api } from '../../utils/api';
 
 export const AddSolicitacao = () => {
+  const navigate = useNavigate();
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const error = () => {
+    messageApi.open({
+      type: 'error',
+      content: 'Não foi possível cadastrar a solicitação',
+    });
+  };
+
+  const cadastrarSolicitacao = async (dados) => {
+    try {
+      await api.post('/solicitacoes', dados);
+      navigate('/');
+    } catch (e) {
+      error();
+    }
+  };
+
   return (
     <LayoutDefault>
-      <Form layout="vertical">
+      {contextHolder}
+      <Form layout="vertical" onFinish={cadastrarSolicitacao}>
         <Row justify="center">
           <Col span={20}>
             <Link to="/">
@@ -23,7 +46,7 @@ export const AddSolicitacao = () => {
           </Col>
 
           <Col span={20}>
-            <Typography.Title level={4}>
+            <Typography.Title level={3}>
               Adicionar nova solicitação
             </Typography.Title>
           </Col>
@@ -32,13 +55,41 @@ export const AddSolicitacao = () => {
             <TituloCategoria>Empresa</TituloCategoria>
           </Categoria>
           <Col span={20}>
-            <Form.Item label="Nome da empresa">
+            <Form.Item
+              label="Nome da empresa"
+              name="nomeEmpresa"
+              rules={[
+                { required: true, message: 'O nome da empresa é obrigatório' },
+              ]}
+            >
               <Input />
             </Form.Item>
-            <Form.Item label="Faturamento anual">
+            <Form.Item
+              label="Faturamento anual"
+              name="faturamentoAnual"
+              rules={[
+                {
+                  required: true,
+                  message: 'O faturamento anual é obrigatório',
+                },
+              ]}
+            >
               <Input />
             </Form.Item>
-            <Form.Item label="Valor solicitado">
+            <Form.Item
+              label="Valor solicitado"
+              name="valorSolicitado"
+              rules={[
+                { required: true, message: 'O valor solicitado é obrigatório' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="CNPJ"
+              name="cnpj"
+              rules={[{ required: true, message: 'O CNPJ é obrigatório' }]}
+            >
               <Input />
             </Form.Item>
           </Col>
@@ -47,32 +98,32 @@ export const AddSolicitacao = () => {
             <TituloCategoria>Endereço</TituloCategoria>
           </Categoria>
           <Col span={20}>
-            <Form.Item label="CEP">
+            <Form.Item label="CEP" name="cep">
               <Input />
             </Form.Item>
             <Row justify="space-between" gutter={8}>
               <Col span={16}>
-                <Form.Item label="Rua">
+                <Form.Item label="Rua" name="rua">
                   <Input />
                 </Form.Item>
               </Col>
               <Col span={8}>
-                <Form.Item label="Número">
+                <Form.Item label="Número" name="numero">
                   <Input />
                 </Form.Item>
               </Col>
             </Row>
-            <Form.Item label="Bairro">
+            <Form.Item label="Bairro" name="bairro">
               <Input />
             </Form.Item>
             <Row justify="space-between" gutter={8}>
               <Col span={16}>
-                <Form.Item label="Cidade">
+                <Form.Item label="Cidade" name="cidade">
                   <Input />
                 </Form.Item>
               </Col>
               <Col span={8}>
-                <Form.Item label="Estado">
+                <Form.Item label="Estado" name="estado">
                   <Input />
                 </Form.Item>
               </Col>
@@ -83,22 +134,60 @@ export const AddSolicitacao = () => {
             <TituloCategoria>Contato</TituloCategoria>
           </Categoria>
           <Col span={20}>
-            <Form.Item label="Nome completo">
+            <Form.Item
+              label="Nome completo"
+              name="nomeResponsavel"
+              rules={[
+                {
+                  required: true,
+                  message: 'O nome completo é obrigatório',
+                },
+              ]}
+            >
               <Input />
             </Form.Item>
-            <Form.Item label="Telefone de contato">
+            <Form.Item
+              label="Telefone de contato"
+              name="telefone"
+              rules={[
+                {
+                  required: true,
+                  message: 'O telefone é obrigatório',
+                },
+              ]}
+            >
               <Input />
             </Form.Item>
-            <Form.Item label="Email">
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'O email é obrigatório',
+                },
+              ]}
+            >
               <Input />
             </Form.Item>
-            <Form.Item label="CPF">
+            <Form.Item
+              label="CPF"
+              name="cpf"
+              rules={[
+                {
+                  required: true,
+                  message: 'O CPF é obrigatório',
+                },
+              ]}
+            >
               <Input />
             </Form.Item>
           </Col>
 
           <Col span={20}>
-            <Button block>Criar nova solicitação</Button>
+            <BotaoForm block htmlType="submit">
+              Criar nova solicitação
+            </BotaoForm>
           </Col>
         </Row>
       </Form>
