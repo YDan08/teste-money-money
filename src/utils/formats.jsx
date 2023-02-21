8f636196ae5ea86b57c8b720cdb76d94f0344838
YDan08 <dasyamashita@gmail.com>
2023-02-21 17:24:41 -0300

    feat: criar os formats

---
+++ src/utils/formats.jsx
@@ -0,0 +1,45 @@
+export const formatCPFInput = (value) => {
+  return value
+    .trim()
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const formatCNPJInput = (value) => {
+  return value
+    .trim()
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const formatCEPInput = (value) => {
+  return value
+    .trim()
+    .replace(/\D/g, '')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{3})\d+?$/, '$1');
+};
+
+export const formatTelefoneInput = (value) => {
+  if (value.length > 14) {
+    return value
+      .trim()
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{4})\d+?$/, '$1');
+  }
+  return value
+    .trim()
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
+};
