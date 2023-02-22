fd25fffe1a0dae4b8ec9378e4c34e0b482b584c6
YDan08 <dasyamashita@gmail.com>
2023-02-22 01:23:45 -0300

    feat: adicionar validators

--- src/utils/validators.jsx
+++ src/utils/validators.jsx
@@ -3,11 +3,38 @@ import * as yup from 'yup';
 export const validarEmail = async (email) =>
   yup.string().email('Digite um email válido').validate(email);
 
-export const validarCpf = (cpf) =>
+export const validarCPF = (cpf) =>
+  cpf.length > 0 &&
   yup
     .string()
     .matches(
-      /([0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2})/,
+      /([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2})/,
       'Digite um CPF válido'
     )
     .validate(cpf);
+
+export const validarCNPJ = (cnpj) =>
+  yup
+    .string()
+    .matches(
+      /([0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2})/,
+      'Digite um CNPJ válido'
+    )
+    .validate(cnpj);
+
+export const validarCEP = (cep) =>
+  cep.length > 0 &&
+  yup
+    .string()
+    .matches(/([0-9]{5}[-]?[0-9]{3})/, 'Digite um CEP válido')
+    .validate(cep);
+
+export const validarTelefone = (telefone) =>
+  telefone.length > 0 &&
+  yup
+    .string()
+    .matches(
+      /([(]?[0-9]{2}[)]?[ ]?[0-9]{5}[-]?[0-9]{4})|([(]?[0-9]{2}[)]?[ ]?[0-9]{4}[-]?[0-9]{4})/,
+      'Digite um telefone válido'
+    )
+    .validate(telefone);
